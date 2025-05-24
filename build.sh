2c5e60d5c2ed7d0dd68caf85fd92e3b1a791ebf9
aayush93407 <92848064+aayush93407@users.noreply.github.com>
2025-05-24 18:31:12 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Install Chrome
+apt-get update && apt-get install -y wget gnupg unzip
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get update && apt-get install -y google-chrome-stable
+
+# Set CHROME_BIN for Selenium
+export CHROME_BIN="/usr/bin/google-chrome"
+
+# Verify install
+google-chrome --version
